f4e4602750724ca0a2c286611455c2a76b6f95bc
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-06-21 21:15:02 -0300

    correcao-code-smell

--- src/app/_06-rotas/rotas/cursos/cursos.module.ts
+++ src/app/_06-rotas/rotas/cursos/cursos.module.ts
@@ -1,11 +1,10 @@
-import { CursosRoutingModule } from './cursos.routing.module';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosServiceService } from './cursos-service.service';
 import { CursosRotasComponent } from './cursos.component';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 @NgModule({
     declarations: [
